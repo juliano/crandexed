c7f10b25a4f79201a6263f36bc61eec01ef69cd4
Juliano Alves <von.juliano@gmail.com>
2015-02-22 10:04:10 -0300

    getting all information about r packages

--- app/models/cran_packages.rb
+++ app/models/cran_packages.rb
@@ -1,10 +1,18 @@
 require 'dcf'
+require 'open-uri'
+require 'rubygems/package'
 
 class CranPackages
   URL = URI.parse('http://cran.r-project.org/src/contrib/PACKAGES')
 
+  def initialize
+    OpenURI::Buffer.send(:remove_const, 'StringMax')
+    OpenURI::Buffer.const_set('StringMax', 0)
+  end
+
   def all
     packages = basic_information.map {|params| Package.new(params) }
+    packages.each {|p| fill_details(p) }
   end
 
   private
@@ -16,5 +24,23 @@ class CranPackages
     rescue
       Kernel.puts("It was not possible to access cran site.")
     end
+  end
+
+  def fill_details(package)
+    params = build_params(details_of(package))
+    package.assign_attributes(params)
+  end
+
+  def details_of(package)
+    data = nil
+    open(package.url) do |remote_file|
+      tar = Gem::Package::TarReader.new(Zlib::GzipReader.open(remote_file))
+      data = tar.seek("#{package.name}/DESCRIPTION") {|d| Dcf.parse(d.read)[0] }
+    end
+    data
+  end
+
+  def build_params(data)
+    { publication: data["Date/Publication"], title: data["Title"], description: data["Description"], authors: data["Author"], maintainers: data["Maintainer"] }
   end
 end
