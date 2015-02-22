b790db1831b170aa0ecaf7863633b883cbeba5e5
Juliano Alves <von.juliano@gmail.com>
2015-02-22 09:03:38 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crandexed::Application.config.secret_key_base = 'cc2ac64898d09b0df385232aaba9d16ac85fe1e25b3de7dd5aa3f4bcea15942f02891d235478b752aa4159e367fa883e29f943722d1ef3f1351d5d9df6de9642'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crandexed::Application.config.session_store :cookie_store, key: '_crandexed_session'
