487be47d18940eb223447a08e3398511884b2d48
Juliano Alves <von.juliano@gmail.com>
2015-02-22 10:40:44 -0300

    package knows when itself exists in the base

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,4 +4,8 @@ class Package < ActiveRecord::Base
     "http://cran.r-project.org/src/contrib/#{name}_#{version}.tar.gz"
   end
 
+  def exists?
+    Package.where("name = ? and version = ?", name, version).present?
+  end
+
 end
--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -6,4 +6,15 @@ describe Package do
   it "knows it's own url" do
     expect(subject.url).to eq "http://cran.r-project.org/src/contrib/SpongeBob_2.2.2.tar.gz"
   end
+
+  describe "#exists?" do
+    it "knows when itself exists in the base" do
+      subject.save
+      expect(subject.exists?).to be true
+    end
+
+    it "knows when itself doesn't exist in the base" do
+      expect(subject.exists?).to be false
+    end
+  end
 end
