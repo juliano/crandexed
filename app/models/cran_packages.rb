c13379e7721617752312e099d9732067efe98fe8
Juliano Alves <von.juliano@gmail.com>
2015-02-22 09:27:52 -0300

    creating Package with basic information

--- app/models/cran_packages.rb
+++ app/models/cran_packages.rb
@@ -1,11 +1,18 @@
 require 'dcf'
 
 class CranPackages
+  URL = URI.parse('http://cran.r-project.org/src/contrib/PACKAGES')
 
-  def get_information
+  def all
+    packages = basic_information.map {|params| Package.new(params) }
+  end
+
+  private
+  def basic_information
     begin
-      packages_info = Net::HTTP.get(URI.parse('http://cran.r-project.org/src/contrib/PACKAGES'))
+      packages_info = Net::HTTP.get(URL)
       packages = Dcf.parse(packages_info)[0, 10]
+      packages.map {|p| { name: p["Package"], version: p["Version"] } }
     rescue
       Kernel.puts("It was not possible to access cran site.")
     end
